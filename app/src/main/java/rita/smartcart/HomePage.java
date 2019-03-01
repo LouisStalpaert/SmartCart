d5b4629c43e296d6fb01858871ce3665f83fe664
Louis Stalpaert <louis.stalpaert@gmail.com>
2019-03-01 23:14:49 +0100

    Version 1

---
+++ app/src/main/java/rita/smartcart/HomePage.java
@@ -0,0 +1,17 @@
+package rita.smartcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    public Boolean Testclass(){
+        return false;
+    }
+}
