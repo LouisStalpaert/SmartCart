fc8e2afd2a4acc5f785e900fdea34bd195c2d0ef
PieterZwaluw <pieter.swaelens@hotmail.com>
2019-03-02 16:43:38 +0100

    Vriendschap

--- app/src/main/java/rita/smartcart/MijnGroep.java
+++ app/src/main/java/rita/smartcart/MijnGroep.java
@@ -15,15 +15,5 @@ public class MijnGroep extends AppCompatActivity {
         setContentView(R.layout.activity_mijn_groep);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
